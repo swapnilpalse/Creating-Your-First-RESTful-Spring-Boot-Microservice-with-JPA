02a5a3b9feada2ffc0d12db865562494ad76adfd
swapnil palse <swapnil.palse@gmail.com>
2018-05-13 15:38:30 -0700

    Updated the TourRating Controller with getAllRatingsForTour and getAverage methods

--- src/main/java/com/swapnil/microservices/web/TourRatingController.java
+++ src/main/java/com/swapnil/microservices/web/TourRatingController.java
@@ -1,7 +1,10 @@
 package com.swapnil.microservices.web;
 
-
+import java.util.AbstractMap;
+import java.util.List;
 import java.util.NoSuchElementException;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -23,66 +26,68 @@ import com.swapnil.microservices.repository.TourRepository;
 @RestController
 @RequestMapping(path = "/tours/{tourId}/ratings")
 public class TourRatingController {
-    TourRatingRepository tourRatingRepository;
-    TourRepository tourRepository;
+	TourRatingRepository tourRatingRepository;
+	TourRepository tourRepository;
 
-    @Autowired
-    public TourRatingController(TourRatingRepository tourRatingRepository, TourRepository tourRepository) {
-        this.tourRatingRepository = tourRatingRepository;
-        this.tourRepository = tourRepository;
-    }
+	@Autowired
+	public TourRatingController(TourRatingRepository tourRatingRepository, TourRepository tourRepository) {
+		this.tourRatingRepository = tourRatingRepository;
+		this.tourRepository = tourRepository;
+	}
 
-    public TourRatingController() {
+	public TourRatingController() {
 
-    }
-    @RequestMapping(method=RequestMethod.POST)
-    @ResponseStatus(HttpStatus.CREATED)
-    public void createTourRating(@PathVariable(value="tourId")int tourId,@RequestBody @Validated RatingDto ratingDto) {
-    	Tour tour=verifyTour(tourId);
-    	tourRatingRepository.save(new TourRating(new TourRatingPk(tour, ratingDto.getCustomerId()), ratingDto.getScore(), ratingDto.getComment()));
-    }
+	}
 
-    /**
-     * Convert the TourRating entity to a RatingDto
-     *
-     * @param tourRating
-     * @return RatingDto
-     */
-    private RatingDto toDto(TourRating tourRating) {
-        return new RatingDto(tourRating.getScore(), tourRating.getComment(), tourRating.getPk().getCustomerId());
-    }
+	@RequestMapping(method = RequestMethod.POST)
+	@ResponseStatus(HttpStatus.CREATED)
+	public void createTourRating(@PathVariable(value = "tourId") int tourId,
+			@RequestBody @Validated RatingDto ratingDto) {
+		Tour tour = verifyTour(tourId);
+		tourRatingRepository.save(new TourRating(new TourRatingPk(tour, ratingDto.getCustomerId()),
+				ratingDto.getScore(), ratingDto.getComment()));
+	}
 
-    /**
-     * Verify and return the TourRating for a particular tourId and Customer
-     * @param tourId
-     * @param customerId
-     * @return the found TourRating
-     * @throws NoSuchElementException if no TourRating found
-     */
-    private TourRating verifyTourRating(int tourId, int customerId) throws NoSuchElementException {
-        TourRating rating = tourRatingRepository.findByPkTourIdAndPkCustomerId(tourId, customerId);
-        if (rating == null) {
-            throw new NoSuchElementException("Tour-Rating pair for request("
-                    + tourId + " for customer" + customerId);
-        }
-        return rating;
-    }
+	@RequestMapping(method = RequestMethod.GET)
+	public List<RatingDto> getAllRatingsForTour(@PathVariable(value = "tourId") int tourId) {
+		verifyTour(tourId);
+		return tourRatingRepository.findByPkTourId(tourId).stream().map(tourRating -> toDto(tourRating))
+				.collect(Collectors.toList());
+	}
+@RequestMapping(method=RequestMethod.GET,path="/average")
+	public AbstractMap.SimpleEntry<String, Double> getAverage(@PathVariable(value = "tourId") int tourId) {
+		verifyTour(tourId);
+		List<TourRating> tourRatings = tourRatingRepository.findByPkTourId(tourId);
+		OptionalDouble average = tourRatings.stream().mapToInt(TourRating::getScore).average();
+		return new AbstractMap.SimpleEntry<String, Double>("avergae",
+				average.isPresent() ? average.getAsDouble() : null);
+	}
 
-  
-    private Tour verifyTour(int tourId) throws NoSuchElementException {
-        Tour tour = tourRepository.findById(tourId).get();
-        if (tour == null) {
-            throw new NoSuchElementException("Tour does not exist " + tourId);
-        }
-        return tour;
-    }
+	private RatingDto toDto(TourRating tourRating) {
+		return new RatingDto(tourRating.getScore(), tourRating.getComment(), tourRating.getPk().getCustomerId());
+	}
 
-   
-    @ResponseStatus(HttpStatus.NOT_FOUND)
-    @ExceptionHandler(NoSuchElementException.class)
-    public String return400(NoSuchElementException ex) {
-        return ex.getMessage();
+	private TourRating verifyTourRating(int tourId, int customerId) throws NoSuchElementException {
+		TourRating rating = tourRatingRepository.findByPkTourIdAndPkCustomerId(tourId, customerId);
+		if (rating == null) {
+			throw new NoSuchElementException("Tour-Rating pair for request(" + tourId + " for customer" + customerId);
+		}
+		return rating;
+	}
 
-    }
+	private Tour verifyTour(int tourId) throws NoSuchElementException {
+		Tour tour = tourRepository.findById(tourId).get();
+		if (tour == null) {
+			throw new NoSuchElementException("Tour does not exist " + tourId);
+		}
+		return tour;
+	}
+
+	@ResponseStatus(HttpStatus.NOT_FOUND)
+	@ExceptionHandler(NoSuchElementException.class)
+	public String return400(NoSuchElementException ex) {
+		return ex.getMessage();
+
+	}
 
 }
