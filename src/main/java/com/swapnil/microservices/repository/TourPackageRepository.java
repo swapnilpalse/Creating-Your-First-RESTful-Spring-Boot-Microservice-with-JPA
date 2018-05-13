5a24809da976130a4c454dbd45479abeaee7448a
swapnil palse <swapnil.palse@gmail.com>
2018-05-12 22:01:17 -0700

    Added the Rest Services, restrictions on repository

--- src/main/java/com/swapnil/microservices/repository/TourPackageRepository.java
+++ src/main/java/com/swapnil/microservices/repository/TourPackageRepository.java
@@ -1,9 +1,39 @@
 package com.swapnil.microservices.repository;
 
+
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.swapnil.microservices.bo.TourPackage;
-
+@RepositoryRestResource(collectionResourceRel="packages", path="packages")
 public interface TourPackageRepository extends CrudRepository<TourPackage, String>{
-TourPackage findByName(String name);
+
+
+	TourPackage findByName(@Param("name")String name);
+
+	@Override
+	@RestResource(exported=false)
+	void delete(TourPackage arg0);
+
+	@Override
+	@RestResource(exported=false)
+	void deleteAll();
+
+	@Override
+	@RestResource(exported=false)
+	void deleteAll(Iterable<? extends TourPackage> arg0);
+
+
+	@Override
+	@RestResource(exported=false)
+	<S extends TourPackage> S save(S arg0);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends TourPackage> Iterable<S> saveAll(Iterable<S> arg0);
+	
+	
 }
--- src/main/java/com/swapnil/microservices/repository/TourRepository.java
+++ src/main/java/com/swapnil/microservices/repository/TourRepository.java
@@ -1,9 +1,37 @@
 package com.swapnil.microservices.repository;
 
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import com.swapnil.microservices.bo.Tour;
 
-public interface TourRepository extends CrudRepository<Tour, Long>{
+public interface TourRepository extends PagingAndSortingRepository<Tour, Long> {
+	Page<Tour> findByTourPackageCode(@Param("code") String code, Pageable pageable);
 
+	@Override
+	@RestResource(exported=false)
+	void delete(Tour arg0);
+	@Override
+	@RestResource(exported=false)
+void deleteAll() ;
+
+	@Override
+	@RestResource(exported=false)
+	void deleteAll(Iterable<? extends Tour> arg0) ;
+
+	@Override
+	@RestResource(exported=false)
+	void deleteById(Long arg0);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends Tour> S save(S arg0);
+
+	@Override
+	@RestResource(exported=false)
+	<S extends Tour> Iterable<S> saveAll(Iterable<S> arg0);
+	
 }
